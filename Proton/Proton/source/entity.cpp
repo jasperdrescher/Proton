fbd1328c632b42ec1b839542b0e8bc53ce45bf18
JasperDre <jasperdre@gmail.com>
2018-03-03 21:46:58 +0100

    [ADDED] Entity names

--- Proton/Proton/source/entity.cpp
+++ Proton/Proton/source/entity.cpp
@@ -1,6 +1,10 @@
 #include "entity.h"
 
-Entity::Entity()
+Entity::Entity() : m_Name("")
+{
+}
+
+Entity::Entity(const std::string & a_Name) : m_Name(a_Name)
 {
 }
 
@@ -18,4 +22,41 @@ void Entity::Render()
 
 void Entity::Destroy()
 {
+}
+
+const std::string & Entity::GetName() const
+{
+	return m_Name;
+}
+
+void Entity::SetName(const std::string & a_Name)
+{
+	m_Name = a_Name;
+}
+
+bool Entity::CompareName(const std::string & a_Name)
+{
+	if (m_Name == a_Name)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+Entity::Entity(const Entity & a_Entity)
+{
+	m_Name = a_Entity.m_Name;
+}
+
+Entity & Entity::operator=(const Entity & a_Entity)
+{
+	if (this != &a_Entity)
+	{
+		m_Name = a_Entity.GetName();
+	}
+
+	return *this;
 }
--- Proton/Proton/source/entity.h
+++ Proton/Proton/source/entity.h
@@ -1,18 +1,27 @@
 #pragma once
 
 #include <glm\vec3.hpp>
+#include <string>
 
 class Entity
 {
 public:
 	Entity();
+	Entity(const std::string& a_Name);
+	Entity(const Entity& a_Entity);
+	Entity& operator=(const Entity& a_Entity);
 	~Entity();
 
 	void Update(float a_DeltaTime);
 	void Render();
 	void Destroy();
 
-	glm::vec3 m_Position;
+	const std::string& GetName() const;
+	void SetName(const std::string& a_Name);
+	bool CompareName(const std::string& a_Name);
 
 private:
+	unsigned int m_ID;
+	std::string m_Name;
+	glm::vec3 m_Position;
 };
