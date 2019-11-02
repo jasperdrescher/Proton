695d5833b38f5ddcbeb7c888079e8e692e1b0edc
JasperDre <jasperdre@gmail.com>
2019-11-02 14:55:05 +0100

    Update Engine.cpp

--- source/Core/Engine.cpp
+++ source/Core/Engine.cpp
@@ -78,6 +78,42 @@ namespace Proton
             return false;
         }
 
+		// RENDER
+		GLuint VertexArrayID;
+		glGenVertexArrays(1, &VertexArrayID);
+		glBindVertexArray(VertexArrayID);
+
+		static const GLfloat g_vertex_buffer_data[] = {
+		   -1.0f, -1.0f, 0.0f,
+		   1.0f, -1.0f, 0.0f,
+		   0.0f,  1.0f, 0.0f,
+		};
+
+		// This will identify our vertex buffer
+		GLuint vertexbuffer;
+		// Generate 1 buffer, put the resulting identifier in vertexbuffer
+		glGenBuffers(1, &vertexbuffer);
+		// The following commands will talk about our 'vertexbuffer' buffer
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		// Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+		// END
+
         myEditor = new Editor();
 		myEditor->SetWindow(myWindow);
 		myEditor->Initialize();
@@ -128,7 +164,7 @@ namespace Proton
 
         if (glfwWindowShouldClose(GetWindow()))
         {
-            myWindowShouldClose = false;
+            myWindowShouldClose = true;
         }
 
         return true;
